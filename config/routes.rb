df5d9856e2f327385c88f909afbf97de052d38ce
Matthias Kutz <matthias.kutz@gmx.net>
2021-02-18 16:34:58 +0100

    Routing, Controller, Views for Cocktails added and working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # root to: "cocktails#index"
+
+  # A user can see the list of cocktails
+  get "cocktails", to: "cocktails#index"
+
+  # A user can create a new cocktail
+  get "cocktails/new", to: "cocktails#new", as: :new_cocktail
+  post "cocktails", to: "cocktails#create"
+
+  # A user can see the details of a given cocktail,
+  # with the dose needed for each ingredient
+  get "cocktails/:id", to: "cocktails#show", as: :cocktail
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating ingredients..."
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "sugar")
+Ingredient.create(name: "lime juice")
+Ingredient.create(name: "tea")
+Ingredient.create(name: "grapes")
+Ingredient.create(name: "water")
+puts "Finished!"
